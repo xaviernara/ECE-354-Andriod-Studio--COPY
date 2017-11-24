e3922e8140c2d8171291f33bc953ed2519600a1d
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2017-11-23 19:42:03 -0600

    STILL messing with this calc but whatever

--- ECE-354-Andriod-Studio--master/Andriod Studio Examples/Calculator/app/src/main/java/com/simple_calc/xarichar/calculator/MainActivity.java
+++ ECE-354-Andriod-Studio--master/Andriod Studio Examples/Calculator/app/src/main/java/com/simple_calc/xarichar/calculator/MainActivity.java
@@ -11,7 +11,24 @@ import android.view.View;
 import android.widget.Button;
 import android.util.Log;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+    private double valueOne = Double.NaN;
+    private double valueTwo = Double.NaN;
+
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char MULTIPLICATION = '*';
+    private static final char DIVISION = '/';
+    private static final char POWER = '^';
+    private static final char SQRT = '~';
+    // CURRENT_ACTION variable, which holds the next operation we intend to perform.
+
+    private char CURRENT_ACTION;
+
+
+
     private ActivityBinding binding;
 
     private TextView mTextMessage;
@@ -43,89 +60,233 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         /* setContentView(R.layout.activity_main); */
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        decimalFormat = new DecimalFormat("##.#######");
 
+
+/* Add Button and click operations */
         Button AddButton = (Button)findViewById(R.id.addbutton);
-        AddButton.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v){
-                        TextView AddText = (TextView)findViewById(R.id.operand_1_Text);
-                        TextView AddText2 = (TextView)findViewById(R.id.operand_2_Text);
-                        TextView AddText3 = (TextView)findViewById(R.id.result_Text);
-                        AddText.setText("TESTTING +");
-                    }
-                });
+        binding.AddButton.setOnClickListener(new View.OnClickListener(){
+       // AddButton.setOnClickListener(
+            @Override
+        //View.OnClickListener = new Button.OnClickListener() {
+            public void onClick(View v) {
 
+                binding.editText.setText(binding.editText.getText() + "Add" );
+                //TextView AddText = (TextView) findViewById(R.id.operand_1_Text);
+                //TextView AddText2 = (TextView) findViewById(R.id.operand_2_Text);
+                //TextView AddText3 = (TextView) findViewById(R.id.result_Text);
+                //AddText.setText("TESTTING +");
+            }
+        });
+
+/* Subtract Button and click operations */
         Button SubtractButton = (Button)findViewById(R.id.subtractbutton);
-        SubtractButton.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v){
-                        TextView SubtractText = (TextView)findViewById(R.id.operand_1_Text);
-                        TextView SubtractText2 = (TextView)findViewById(R.id.operand_2_Text);
-                        TextView SubtractText3 = (TextView)findViewById(R.id.result_Text);
-                        AddText.setText("TESTTING -");
+        //SubtractButton.setOnClickListener(
+        binding.SubtractButton.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                // new Button.OnClickListener(){
+        public void onClick(View v){
+                //TextView SubtractText = (TextView)findViewById(R.id.operand_1_Text);
+                //TextView SubtractText2 = (TextView)findViewById(R.id.operand_2_Text);
+                //TextView SubtractText3 = (TextView)findViewById(R.id.result_Text);//
+                    //AddText.setText("TESTTING -");
+                    binding.editText.setText(binding.editText.getText() + "Subtract");
                     }
                 });
 
+
+/* Multiply Button and click operations */
         Button MultiplyButton = (Button)findViewById(R.id.multiplybutton);
-        MultiplyButton.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v){
-                        TextView MultiplyText = (TextView)findViewById(R.id.operand_1_Text);
-                        TextView MultiplyText2 = (TextView)findViewById(R.id.operand_2_Text);
-                        TextView MultiplyText3 = (TextView)findViewById(R.id.result_Text);
-                        AddText.setText("TESTTING *");
-                    }
-                });
+        binding.MultiplyButton.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            /* new Button.OnClickListener(){ */
+
+            public void onClick(View v){
+                //MultiplyButton.setOnClickListener(
+
+                //TextView MultiplyText = (TextView)findViewById(R.id.operand_1_Text);
+                //TextView MultiplyText2 = (TextView)findViewById(R.id.operand_2_Text);
+                //TextView MultiplyText3 = (TextView)findViewById(R.id.result_Text);
+                //AddText.setText("TESTTING *");
+                binding.editText.setText(binding.editText.getText() + "Subtract");
+            }
+    });
+
+
+
+    /* Divide Button and click operations */
         Button DivideButton = (Button)findViewById(R.id.dividebutton);
-        DivideButton.setOnClickListener(
-                new Button.OnClickListener(){
+        binding.DivideButton.setOnClickListener(new View.OnClickListener(){
+       // DivideButton.setOnClickListener(
+                //new Button.OnClickListener(){
                     public void onClick(View v){
-                        TextView DivideText = (TextView)findViewById(R.id.operand_1_Text);
-                        TextView DivideText2 = (TextView)findViewById(R.id.operand_2_Text);
-                        TextView DivideText3 = (TextView)findViewById(R.id.result_Text);
-                        AddText.setText("TESTTING / ");
+                        //TextView DivideText = (TextView)findViewById(R.id.operand_1_Text);
+                        //TextView DivideText2 = (TextView)findViewById(R.id.operand_2_Text);
+                        //TextView DivideText3 = (TextView)findViewById(R.id.result_Text);
+                        //+AddText.setText("TESTTING / ");
+                    binding.editText.setText(binding.editText.getText() + "Divide");
                     }
                 });
 
+
+
+    /* Sqrt Button and click operations */
         Button SqrtButton = (Button)findViewById(R.id.sqrtbutton);
-        SqrtButton.setOnClickListener(
-                new Button.OnClickListener(){
+        binding.SqrtButton.setOnClickListener(new View.OnClickListener(){
+        //SqrtButton.setOnClickListener(
                     public void onClick(View v){
-                        TextView SqrtText = (TextView)findViewById(R.id.operand_1_Text);
-                        TextView SqrtText2 = (TextView)findViewById(R.id.operand_2_Text);
-                        TextView SqrtText3 = (TextView)findViewById(R.id.result_Text);
-                        AddText.setText("TESTTING sqrt");
+                        //TextView SqrtText = (TextView)findViewById(R.id.operand_1_Text);
+                        //TextView SqrtText2 = (TextView)findViewById(R.id.operand_2_Text);
+                        //TextView SqrtText3 = (TextView)findViewById(R.id.result_Text);
+                        //AddText.setText("TESTTING sqrt");
+                         binding.editText.setText(binding.editText.getText() + "SQRT");
                     }
                 });
 
+
+
+
+      /* Power Button and click operations */
         Button PowerButton = (Button)findViewById(R.id.powerbutton);
-        PowerButton.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v){
-                        TextView PowerText = (TextView)findViewById(R.id.operand_1_Text);
-                        TextView PowerText2 = (TextView)findViewById(R.id.operand_2_Text);
-                        TextView PowerText3 = (TextView)findViewById(R.id.result_Text);
-                        AddText.setText("TESTTING ^");
-                    }
-                });
+        //PowerButton.setOnClickListener(
+        binding.PowerButton.setOnClickListener(new View.OnClickListener(){
+       //new Button.OnClickListener(){
+        public void onClick(View v){
+            //TextView PowerText = (TextView)findViewById(R.id.operand_1_Text);
+            //TextView PowerText2 = (TextView)findViewById(R.id.operand_2_Text);
+            //TextView PowerText3 = (TextView)findViewById(R.id.result_Text);
+            //AddText.setText("TESTTING ^");
+            binding.editText.setText(binding.editText.getText() + "POWER");
+        }
+        });
 
+
+
+
+     /* Power Button and click operations */
         Button ClearButton = (Button)findViewById(R.id.clearbutton);
-        ClearButton.setOnClickListener(
-                new Button.OnClickListener(){
+        binding.ClearButton.setOnClickListener(new View.OnClickListener(){
+        //ClearButton.setOnClickListener(
+                //new Button.OnClickListener(){
                     public void onClick(View v){
-                        TextView AddText = (TextView)findViewById(R.id.operand_1_Text);
-                        TextView AddText2 = (TextView)findViewById(R.id.operand_2_Text);
-                        TextView AddText3 = (TextView)findViewById(R.id.result_Text);
-                        AddText.setText("TESTTING");
+                        //TextView PowerText = (TextView)findViewById(R.id.operand_1_Text);
+                        //TextView PowerText2 = (TextView)findViewById(R.id.operand_2_Text);
+                        //TextView PowerText3 = (TextView)findViewById(R.id.result_Text);
+                        //AddText.setText("TESTTING clear");
+                        binding.editText.setText(binding.editText.getText() + "CLEAR");
                     }
                 });
 
+        public void computeCalculator(){
+            if(!Double.isNaN(valueOne)) {
+                valueTwo = Double.parseDouble(binding.editText.getText().toString());
+                binding.editText.setText(null);
+
+                if(CURRENT_ACTION == ADDITION)
+                    valueOne = this.valueOne + valueTwo;
+                else if(CURRENT_ACTION == SUBTRACTION)
+                    valueOne = this.valueOne - valueTwo;
+                else if(CURRENT_ACTION == MULTIPLICATION)
+                    valueOne = this.valueOne * valueTwo;
+                else if(CURRENT_ACTION == DIVISION)
+                    valueOne = this.valueOne / valueTwo;
+                    if ( valueTwo == 0 ){
+                        System.out.println("You can't divide by 0");
+                        return;
+                    }
+                else if (CURRENT_ACTION == POWER)
+                    valueOne = Math.pow(valueOne,valueTwo);
+                else if (CURRENT_ACTION== SQRT)
+                    valueOne = Math.sqrt(valueOne);
+                    valueTwo = Math.sqrt(valueTwo);
+
+            }
+            else {
+                try {
+                    valueOne = Double.parseDouble(binding.editText.getText().toString());
+                }
+                catch (Exception e){}
+            }
+        }
 
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        binding.addbutton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            computeCalculation();
+            CURRENT_ACTION = ADDITION;
+            binding.infoTextView.setText(decimalFormat.format(valueOne) + "+");
+            binding.editText.setText(null);
+        }
+    });
+
+        binding.subtractbutton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            computeCalculation();
+            CURRENT_ACTION = SUBTRACTION;
+            binding.infoTextView.setText(decimalFormat.format(valueOne) + "-");
+            binding.editText.setText(null);
+        }
+    });
+
+        binding.multiplybutton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            computeCalculation();
+            CURRENT_ACTION = MULTIPLICATION;
+            binding.infoTextView.setText(decimalFormat.format(valueOne) + "*");
+            binding.editText.setText(null);
+        }
+    });
+
+        binding.dividebutton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            computeCalculation();
+            CURRENT_ACTION = DIVISION;
+            binding.infoTextView.setText(decimalFormat.format(valueOne) + "/");
+            binding.editText.setText(null);
+        }
+    });
+
+
+            binding.sqrtbutton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            computeCalculation();
+            CURRENT_ACTION = DIVISION;
+            binding.infoTextView.setText(decimalFormat.format(valueOne) + "~");
+            binding.editText.setText(null);
+        }
+    });
+
+            binding.powerbutton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            computeCalculation();
+            CURRENT_ACTION = DIVISION;
+            binding.infoTextView.setText(decimalFormat.format(valueOne) + "^");
+            binding.editText.setText(null);
+        }
+    });
+        binding.clearbutton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            computeCalculation();
+            binding.infoTextView.setText(binding.infoTextView.getText().toString() +
+                    decimalFormat.format(valueTwo) + " = " + decimalFormat.format(valueOne));
+            valueOne = Double.NaN;
+            CURRENT_ACTION = '0';
+        }
+    });
+
+
+    mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+}
 
 }
